e6ef0bb0fc39c9e0977d5febcaab39d821a62e79
George Eaton <george_eaton@hotmail.co.uk>
2016-10-17 14:24:22 +0100

    added full name greeting

---
+++ chap05/ex1.rb
@@ -0,0 +1,9 @@
+#Full name greeting
+puts "What's your first name?"
+f_name = gets.chomp
+puts "What's your middle name?"
+m_name = gets.chomp
+puts "What's your surname?"
+s_name = gets.chomp
+
+puts "Hello, #{f_name} #{m_name} #{s_name}!"
