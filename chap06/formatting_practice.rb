33d35466949921fda54157f77d30069fe6605c15
George Eaton <george_eaton@hotmail.co.uk>
2016-10-17 14:53:45 +0100

    added formatting test

---
+++ chap06/formatting_practice.rb
@@ -0,0 +1,6 @@
+line_width = 50
+
+puts ("       This is the first line of a poem,".center(line_width))
+puts ("                Here is the second line,".center(line_width))
+puts ("         I can't think of much to write,".center(line_width))
+puts ("             But this is almost a rhyme.".center(line_width))
