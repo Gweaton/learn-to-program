0cb40446f1e7ae001f61928fa9f0d7d31e9a69b7
George Eaton <george_eaton@hotmail.co.uk>
2016-10-17 12:55:27 +0100

    added mins_in_decade

---
+++ chap02/ex2.rb
@@ -0,0 +1,7 @@
+#Minutes in a decade
+days_in_decade = 10 * 365.25
+mins_in_day = 24 * 60
+
+mins_in_decade = days_in_decade * mins_in_day
+puts mins_in_decade
+#=> 5,259,600 (including estimate for leap years)
