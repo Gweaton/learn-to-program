1c358595aa4ccba468c96015b6ea3568699caf01
George Eaton <george_eaton@hotmail.co.uk>
2016-10-17 14:41:02 +0100

    Added characters_in_name

---
+++ chap06/characters_in_name.rb
@@ -0,0 +1,8 @@
+puts "What's your first name?"
+f_name = gets.chomp
+puts "What's your middle name?"
+m_name = gets.chomp
+puts "What's your surname?"
+s_name = gets.chomp
+
+puts "Did you know there are #{f_name.length + m_name.length + s_name.length} letters in your name?"
