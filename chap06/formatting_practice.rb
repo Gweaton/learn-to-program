2c0c6ad469da17d6b3e18a1cbd18ea72d2b37d74
George Eaton <george_eaton@hotmail.co.uk>
2016-10-17 14:56:27 +0100

    updated formatting practice

--- chap06/formatting_practice.rb
+++ chap06/formatting_practice.rb
@@ -4,3 +4,8 @@ puts ("       This is the first line of a poem,".center(line_width))
 puts ("                Here is the second line,".center(line_width))
 puts ("         I can't think of much to write,".center(line_width))
 puts ("             But this is almost a rhyme.".center(line_width))
+
+puts ("       This is the first line of a poem,".ljust(line_width))
+puts ("                Here is the second line,".rjust(line_width))
+puts ("         I can't think of much to write,".ljust(line_width))
+puts ("             But this is almost a rhyme.".rjust(line_width/2))
