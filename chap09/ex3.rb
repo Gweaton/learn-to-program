1dea3771c22a2ae4fd332eeafea9ddfbd9954524
George Eaton <george_eaton@hotmail.co.uk>
2016-10-18 16:26:54 +0100

    added modern roman numerals

--- chap09/ex3.rb
+++ chap09/ex3.rb
@@ -2,19 +2,43 @@
 def roman_numeral number
   count = ""
 
-  count += "M" * (number / 1000)
-  count += "D" * (number % 1000 / 500)
-  count += "C" * (number % 500 / 100)
-  count += "L" * (number % 100 / 50)
-  count += "X" * (number % 50 / 10)
-  count += "V" * (number % 10 / 5)
-  count += "I" * (number % 5 / 1)
+  thousands = (number / 1000)
+  hundreds = (number % 1000 / 100)
+  tens = (number % 100 / 10)
+  ones = (number % 10)
+
+puts count += 'M' * thousands
+  if hundreds == 9
+    count += 'CM'
+  elsif hundreds == 4
+    count += 'CD'
+  else
+    count += 'D' * (number % 1000 / 500)
+    count += 'C' * (number % 500 / 100)
+  end
+
+  if tens == 9
+    count += 'XC'
+  elsif tens == 4
+    count += 'XL'
+  else
+    count += 'L' * (number % 100 / 50)
+    count += 'X' * (number % 50 / 10)
+  end
+
+  if ones == 9
+    count += 'IX'
+  elsif ones == 4
+    count += 'IV'
+  else
+    count += 'V' * (number % 10 / 5)
+    count += 'I' * (number % 5 / 1)
+  end
 
   puts count
-
 end
 
 puts "Give me a number to turn into Roman Numerals:"
-input = gets.chomp
+input = gets.chomp.to_i
 
 roman_numeral input
