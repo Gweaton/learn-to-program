6a7925b885b9dfcbd0d5e7c98d0e927bb39e1ae8
George Eaton <george_eaton@hotmail.co.uk>
2016-10-18 11:36:40 +0100

    added extended deaf grandma to ex2.rb

--- chap07/ex2.rb
+++ chap07/ex2.rb
@@ -1,18 +1,24 @@
 #Deaf Grandma
 puts "HELLO GRANDSON!"
 bye = "BYE BYE, MY DEAR!"
-chat = " "
+bye_count = 0
 
-while chat != "BYE"
+while true
   chat = gets.chomp!
 
   if chat == "BYE"
+    bye_count += 1
+  else
+    bye_count = 0
+  end
+  if bye_count >= 3
     break
   end
 
   if chat != chat.upcase
     puts "HUH!? SPEAK UP, SONNY!"
-  else year = "19" + (rand(21) + 30).to_s
+  else
+    year = "19" + (rand(21) + 30).to_s
     puts "NO, NOT SINCE #{year}"
   end
 end
