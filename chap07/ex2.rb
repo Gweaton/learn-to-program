87b99833b5b2d7ca1eeb490c5358286affd26ae4
George Eaton <george_eaton@hotmail.co.uk>
2016-10-18 11:25:31 +0100

    fixed issue with deaf grandma

--- chap07/ex2.rb
+++ chap07/ex2.rb
@@ -1,17 +1,19 @@
 #Deaf Grandma
 puts "HELLO GRANDSON!"
 bye = "BYE BYE, MY DEAR!"
-chat = gets.chomp!
+chat = " "
 
 while chat != "BYE"
-  while chat != chat.upcase
-    puts "HUH!? SPEAK UP, SONNY!"
-    chat = gets.chomp!
-  end
-  break if chat == 'BYE' 
-  year = "19" + (rand(21) + 30).to_s
-  puts "NO, NOT SINCE #{year}"
   chat = gets.chomp!
-end
 
+  if chat == "BYE"
+    break
+  end
+
+  if chat != chat.upcase
+    puts "HUH!? SPEAK UP, SONNY!"
+  else year = "19" + (rand(21) + 30).to_s
+    puts "NO, NOT SINCE #{year}"
+  end
+end
 puts bye
